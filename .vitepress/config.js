a31f19dedcffcda10125b7e2b47484455993e321
Xin Tan <mactanxin@gmail.com>
2023-04-10 21:08:22 +0800

    feat(config): vite sidebar updated

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -9,18 +9,39 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
+      { text: 'Svelte', link: '/svelte/' },
       { text: 'About', link: '/about-me' }
     ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: '2023', link: '/2023/' },
-          { text: '2020', link: '/2020/' }
-        ]
-      }
-    ],
+    sidebar: {
+      '/svelte/': [
+        {
+          text: 'Svelte',
+          items: [
+            { text: 'Fetching Data in Sveltekit', link: '/svelte/fetching-data-in-sveltekit' },
+          ]
+        }
+      ],
+      '/2023/': [
+        {
+          text: '2023',
+          items: [
+            { text: '2023 索引', link: '/2023/' },
+            { text: '翻转对象', link: '/2023/convert-object-key-value' }
+          ]
+        }
+      ]
+    },
+
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: '2023', link: '/2023/' },
+    //       { text: '2020', link: '/2020/' }
+    //     ]
+    //   }
+    // ],
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/mactanxin/vitepress-blog' }
