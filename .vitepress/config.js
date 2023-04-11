11cc1345975cfb3a734c69ed870145ae1381cb99
Xin Tan <mactanxin@gmail.com>
2023-04-11 10:35:56 +0800

    feat(config): update side nav

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -20,7 +20,15 @@ export default defineConfig({
           items: [
             { text: 'Fetching Data in Sveltekit', link: '/svelte/fetching-data-in-sveltekit' },
           ]
-        }
+        },
+        {
+          text: '其他',
+          items: [
+            { text: '2023', link: '/2023/' },
+            { text: '2020', link: '/2020/' },
+            { text: '2019', link: '/2019/' },
+          ]
+        },
       ],
       '/2023/': [
         {
@@ -29,8 +37,37 @@ export default defineConfig({
             { text: '2023 索引', link: '/2023/' },
             { text: '翻转对象', link: '/2023/convert-object-key-value' }
           ]
+        },
+        {
+          text: '其他',
+          items: [
+            { text: 'Svelte', link: '/svelte/' },
+            { text: '2020', link: '/2020/' },
+            { text: '2019', link: '/2019/' },
+          ]
+        }
+      ],
+      '/2020/': [
+        {
+          text: '其他',
+          items: [
+            { text: 'Svelte', link: '/svelte/' },
+            { text: '2023', link: '/2023/' },
+            { text: '2019', link: '/2019/' },
+          ]
+        }
+      ],
+      '/2019/': [
+        {
+          text: '其他',
+          items: [
+            { text: 'Svelte', link: '/svelte/' },
+            { text: '2023', link: '/2023/' },
+            { text: '2020', link: '/2020/' },
+          ]
         }
       ]
+
     },
 
     // sidebar: [
